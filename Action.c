fa8218593a3644023ed0ec31c9257ea10f376068
Admin Default <admin@default.com>
2019-04-18 09:50:34 -0600

    added Transaction HomePage

---
+++ Action.c
@@ -0,0 +1,18 @@
+//   *****************************************************************************************************************************************
+//   ****   PLEASE NOTE: This is a READ-ONLY representation of the actual script. For editing please press the "Develop Script" button.   ****
+//   *****************************************************************************************************************************************
+
+Action()
+{
+	lr_start_transaction("HomePage");
+	truclient_step("1", "Navigate to 'http://advantageonlineshopping.com/#/'", "snapshot=Action_1.inf");
+	lr_end_transaction("HomePage",0);
+	truclient_step("2", "Click on SPEAKERS link", "snapshot=Action_2.inf");
+	truclient_step("3", "Click on image (1) image", "snapshot=Action_3.inf");
+	truclient_step("4", "Click on HOME link", "snapshot=Action_4.inf");
+	truclient_step("5", "Click on Shop Now label", "snapshot=Action_5.inf");
+	truclient_step("6", "Click on image (2) image", "snapshot=Action_6.inf");
+	truclient_step("7", "Click on HOME link", "snapshot=Action_7.inf");
+
+	return 0;
+}
